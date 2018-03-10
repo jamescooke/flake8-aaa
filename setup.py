32fd0679348a38b5a04e87e9a85ee541691d3e86
James Cooke <jamescooke@users.noreply.github.com>
2018-03-10 16:21:04 +0000

    Fix install error

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ import setuptools
 
 setuptools.setup(
     name='flake8-aaa',
+
+    # --- META ---
+    description='Flake8 plugin to check the style of Python tests to 3A standard',
+    license='MIT',
+    version='0.0.0',
+
+    # --- Python ---
+    packages=['flake8_aaa'],
+    py_modules=['flake8_aaa'],
     install_requires=[
         'flake8 > 3',
-    ],
-    license='MIT',
-    py_modules=[
-        'flake8_aaa',
+        'py > 1.5',
     ],
     entry_points={
         'flake8.extension': [
