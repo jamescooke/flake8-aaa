2d47b9b7f5cf5420864c2737e8c602d31b1d4366
James Cooke <jamescooke@users.noreply.github.com>
2018-03-15 23:40:39 +0000

    Add failing test for noqa with AAA01

--- tests/test_checker.py
+++ tests/test_checker.py
@@ -16,3 +16,35 @@ def test(flake8dir):
     result = flake8dir.run_flake8()
 
     assert result.out_lines == ['./test_plus.py:3:5: AAA01 no result variable set in test']
+
+
+def test_ignore(flake8dir):
+    flake8dir.make_setup_cfg('''
+        [flake8]
+        ignore = AAA01
+    ''')
+    flake8dir.make_py_files(
+        test_plus='''
+            def test():
+                x = 1 + 1
+                assert x == 2
+        ''',
+    )
+
+    result = flake8dir.run_flake8()
+
+    assert result.out_lines == []
+
+
+def test_noqa(flake8dir):
+    flake8dir.make_py_files(
+        test_plus='''
+            def test():
+                x = 1 + 1  # noqa: AAA01
+                assert x == 2
+        ''',
+    )
+
+    result = flake8dir.run_flake8()
+
+    assert result.out_lines == []
