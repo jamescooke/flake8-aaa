4dccb19b04ff09b3f1f6449bba27463f743c3eca
James Cooke <jamescooke@users.noreply.github.com>
2018-03-24 18:58:24 +0000

    Indent doctests for sphinx formatting

--- flake8_aaa/helpers.py
+++ flake8_aaa/helpers.py
@@ -11,17 +11,18 @@ def is_test_file(filename):
     Returns:
         bool: This looks like a pytest test file.
 
-    1.  Non-test files give False.
-    >>> is_test_file('./test.py')
-    False
-    >>> is_test_file('./helper.py')
-    False
-    >>> is_test_file('tests/conftest.py')
-    False
+    Examples:
+        1.  Non-test files give False.
+        >>> is_test_file('./test.py')
+        False
+        >>> is_test_file('./helper.py')
+        False
+        >>> is_test_file('tests/conftest.py')
+        False
 
-    2.  Finds files that start with 'test_' to be test files.
-    >>> is_test_file('./test_helpers.py')
-    True
+        2.  Finds files that start with 'test_' to be test files.
+        >>> is_test_file('./test_helpers.py')
+        True
     """
     return py.path.local(filename).basename.startswith('test_')
 
