11a50dbc2745caa25480138ce94f45cf97df675d
James Cooke <jamescooke@users.noreply.github.com>
2018-03-28 19:30:56 +0100

    Rebuild first_token fixture to use FileProcessor
    This guarantees that tests receive what FileProcessor is returning -
    list of tuples on py2 and list of TokenInfo on py3 :(

--- tests/marker/conftest.py
+++ tests/marker/conftest.py
@@ -1,11 +1,27 @@
-import six
-import tokenize
-
 import pytest
+from flake8.defaults import MAX_LINE_LENGTH
+from flake8.processor import FileProcessor
+
+
+class FakeOptions:
+    pass
 
 
 @pytest.fixture
-def first_token(code_str):
+def fake_options():
+    """
+    Returns:
+        FakeOptions
+    """
+    options = FakeOptions()
+    options.hang_closing = False
+    options.max_line_length = MAX_LINE_LENGTH
+    options.verbose = False
+    return options
+
+
+@pytest.fixture
+def first_token(code_str, tmpdir, fake_options):
     """
     Args:
         code_str (str): Code to be tokenized.
@@ -14,5 +30,8 @@ def first_token(code_str):
         tuple (py2)
         tokenize.TokenInfo (py3)
     """
-    string_io = six.StringIO(code_str)
-    return list(tokenize.generate_tokens(string_io.readline))[0]
+    code_file = tmpdir.join('code_file.py')
+    code_file.write(code_str)
+    file_processor = FileProcessor(str(code_file), options=fake_options)
+    tokens = file_processor.generate_tokens()
+    return list(tokens)[0]
