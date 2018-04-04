5954d9abed11acff95b5b642fb9a5d9019ff6ff3
James Cooke <jamescooke@users.noreply.github.com>
2018-04-04 22:25:09 +0100

    Clean out function fixture

--- tests/function/conftest.py
+++ tests/function/conftest.py
@@ -1,22 +1,18 @@
-import ast
+import astroid
 
 import pytest
 
 from flake8_aaa.function import Function
-from flake8_aaa.helpers import find_test_functions, load_markers
 
 
 @pytest.fixture
 def function(code_str, file_tokens):
     """
     Args:
-        code_str (str)
+        code_str (str): Should contain only one function which can be
+            extracted.
 
     Returns:
         Function
     """
-    tree = ast.parse(code_str)
-    function_node = find_test_functions(tree)[0]
-    function = Function(function_node)
-    function.pull_markers(load_markers(file_tokens))
-    return function
+    return Function(astroid.extract_node(code_str))
--- tests/function/test_fixtures.py
+++ tests/function/test_fixtures.py
@@ -8,5 +8,6 @@ def test():
 def test(function):
     result = function
 
+    assert result.node.name == 'test'
     assert list(result.markers) == [3]
     assert result.markers[3].token.string == '# AAA act'
