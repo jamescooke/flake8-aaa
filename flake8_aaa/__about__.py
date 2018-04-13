4323034e55158762d5b6773df400752e7b695ef5
James Cooke <jamescooke@users.noreply.github.com>
2018-04-13 16:39:34 +0100

    Fix Checker to use version number from __about__

--- flake8_aaa/__about__.py
+++ flake8_aaa/__about__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import, print_function, unicode_literals
 
-__name__ = 'flake8-aaa'
+__short_name__ = 'aaa'
+__name__ = 'flake8-{}'.format(__short_name__)
 __version__ = '0.1.0'
 
 __author__ = 'James Cooke'
