668f2b766e04a20949bb5e73f27272b41e98adb4
James Cooke <jamescooke@users.noreply.github.com>
2018-06-23 23:22:26 +0100

    Clean out noop tests

--- tests/test_run.py
+++ tests/test_run.py
@@ -34,36 +34,3 @@ def test(flake8dir):
 
     assert len(result.out_lines)
     assert result.out_lines[0] == './test_plus.py:1:1: AAA01 no Act block found in test'
-
-
-def test_ignore(flake8dir):
-    flake8dir.make_setup_cfg('''
-        [flake8]
-        ignore = AAA01
-    ''')
-    flake8dir.make_py_files(
-        test_plus='''
-            def test():
-                x = 1 + 1
-                assert x == 2
-        ''',
-    )
-
-    result = flake8dir.run_flake8()
-
-    assert result.out_lines == []
-
-
-def test_act(flake8dir):
-    flake8dir.make_py_files(
-        test_plus='''
-            def test():
-                x = 1 + 1  # act
-
-                assert x == 2
-        ''',
-    )
-
-    result = flake8dir.run_flake8()
-
-    assert result.out_lines == []
