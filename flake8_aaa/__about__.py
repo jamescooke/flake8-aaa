2928e8fef30da320fef9b3444952c9044611f5a9
James Cooke <jamescooke@users.noreply.github.com>
2018-06-25 18:09:32 +0100

    Bump to v0.3.0

--- flake8_aaa/__about__.py
+++ flake8_aaa/__about__.py
@@ -2,10 +2,10 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 __short_name__ = 'aaa'
 __name__ = 'flake8-{}'.format(__short_name__)
-__version__ = '0.2.0'
+__version__ = '0.3.0'
 
 __author__ = 'James Cooke'
 __copyright__ = '2018, {}'.format(__author__)
 
-__description__ = 'Flake8 plugin that checks Pytest tests follow the Arrange Act Assert pattern'
+__description__ = 'Extends Flake8 to check that tests follow the Arrange Act Assert pattern'
 __email__ = 'github@jamescooke.info'
