8b0d642a5dd2abb507e58bfaaec9e70df76e0a04
James Cooke <jamescooke@users.noreply.github.com>
2018-11-25 21:56:42 +0000

    Add failing tests for Act wrapped in context manager

--- tests/helpers/test_build_footprint.py
+++ tests/helpers/test_build_footprint.py
@@ -6,22 +6,22 @@ from flake8_aaa.helpers import build_footprint
 @pytest.mark.parametrize(
     'code_str, first_line_no, expected_lines', [
         ('''
-def test():
-    with pytest.raises(Exception):
-        do_thing()
+def test():  # 0
+    with pytest.raises(Exception):  # 1
+        do_thing()  # 2
 
-    other_thing()
+    other_thing()  # 4
 ''', 2, set([1, 2])),
         (
             '''
-@pytest.mark.xfail()
-def test_other():
-    result = do_thing(
-        'yes',
-        None,
-    )
+@pytest.mark.xfail()  # 0
+def test_other():  # 1
+    result = do_thing(  # 2
+        'yes',  # 3
+        None,  # 4
+    )  # 5
 
-    assert result is None
+    assert result is None  # 7
 ''', 2, set([2, 3, 4, 5])
         ),
     ]
@@ -32,3 +32,37 @@ def test(first_node_with_tokens, first_line_no, expected_lines):
     result = build_footprint(first_test_node, first_line_no)
 
     assert result == expected_lines
+
+
+@pytest.mark.parametrize(
+    'code_str', [
+        '''
+# Do a test
+
+
+def test(api_client, url):  # 0
+    data = {  # 1
+        'user_id': 0,  # 2
+        'project_permission': 'admin',  # 3
+    }  # 4
+
+    with catch_signal(user_perms_changed) as callback:  # 6
+        result = api_client.put(url, data=data)  # 7
+
+    assert result.status_code == 400
+    assert callback.call_count == 0
+    ''',
+    ]
+)
+def test_context_manager(first_node_with_tokens):
+    """
+    Act block is selected result assignment, which is wrapped in the context
+    manager. The footprint analysis widens the footprint to include the context
+    manager that wraps the act.
+    """
+    context_manager = first_node_with_tokens.body[1]
+    result_assignment = context_manager.body[0]
+
+    result = build_footprint(result_assignment, 5)
+
+    assert result == set([6, 7])
