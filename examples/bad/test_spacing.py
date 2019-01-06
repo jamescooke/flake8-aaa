7829dbe27e7ca29f1f589509527d262903dfe4a4
James Cooke <jamescooke@users.noreply.github.com>
2019-01-06 21:46:49 +0000

    Stub in ignore and bad examples (wip)

---
+++ examples/bad/test_spacing.py
@@ -0,0 +1,10 @@
+def test():  # noqa
+    x = 1  # noqa
+    y = 2  # noqa
+    result = x + y  # noqa
+    assert result == 3  # noqa
+
+
+def test_compact():  # noqa
+    result = 1 + 2  # noqa
+    assert result == 3  # noqa
---
+++ examples/ignore/test_spacing.py
@@ -0,0 +1,10 @@
+def test():
+    x = 1
+    y = 2
+    result = x + y
+    assert result == 3
+
+
+def test_compact():
+    result = 1 + 2
+    assert result == 3
