4823b7f44fb8b022e39a3e7060be61a1d284bd8f
James Cooke <jamescooke@users.noreply.github.com>
2019-02-27 13:07:29 +0000

    Remove unused fixure

--- tests/function/conftest.py
+++ tests/function/conftest.py
@@ -11,13 +11,3 @@ def function(first_node_with_tokens, lines) -> Function:
         to Function.
     """
     return Function(first_node_with_tokens, lines)
-
-
-@pytest.fixture
-def function_with_act_block(function):
-    """
-    Returns:
-        Function: With Act block loaded.
-    """
-    function.check_act()
-    return function
