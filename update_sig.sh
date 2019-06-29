9bc8357e56cc30270e2dc603c4d73a568315d611
James Cooke <jamescooke@users.noreply.github.com>
2019-06-29 11:16:52 +0100

    Echo updated version line in update_sig script

--- update_sig.sh
+++ update_sig.sh
@@ -7,6 +7,9 @@ grep -E "CPython" README.rst
 
 echo "Running tox to generate new line..."
 NEWLINE=$(tox -e py36-install | grep -E "CPython")
+
+echo "NEWLINE is \"$NEWLINE\""
+
 VERSION=$(echo "$NEWLINE" | grep -Eo "aaa: [^,]*")
 
 echo "Updating file..."
