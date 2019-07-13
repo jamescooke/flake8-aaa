1f219c7ad0af3d9732ab81a9dae22af49537f9d0
James Cooke <jamescooke@users.noreply.github.com>
2019-07-13 13:58:11 +0100

    Copy updated noqa examples to black dir

--- examples/good/black/noqa/test_01.py
+++ examples/good/black/noqa/test_01.py
@@ -4,3 +4,11 @@ def test():  # noqa
 
 def test_multi_line_args(math_fixture, *args, **kwargs):  # noqa
     assert 1 + 1 == 2
+
+
+def test_specific():  # noqa: AAA01
+    assert 1 + 1 == 2
+
+
+def test_multi_line_args_specific(math_fixture, *args, **kwargs):  # noqa: AAA01
+    assert 1 + 1 == 2
