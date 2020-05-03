39518d4bd9d41c4ebb4b8ece4a1602b60a2c9e16
James Cooke <jamescooke@users.noreply.github.com>
2020-05-03 16:25:08 +0100

    Add bad examples for with statements

--- examples/bad/test_aaa03.py
+++ examples/bad/test_aaa03.py
@@ -3,3 +3,10 @@ def test():
     result = x**2
 
     assert result == 4
+
+
+def test_b():
+    with open('f.txt') as f:
+        result = f.read()
+
+    assert result == 'Hello World!\n'
--- examples/bad/test_aaa03_04.py
+++ examples/bad/test_aaa03_04.py
@@ -2,3 +2,9 @@ def test():
     x = 1
     result = x**2
     assert result == 4
+
+
+def test_b():
+    with open('f.txt') as f:
+        result = f.read()
+    assert result == 'Hello World!\n'
