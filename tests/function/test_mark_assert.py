3025b171647f80795282c7d6259cb209eec048c5
James Cooke <jamescooke@users.noreply.github.com>
2020-05-08 11:33:17 +0100

    Add failing test for marking assert with comment in Act

--- tests/function/test_mark_assert.py
+++ tests/function/test_mark_assert.py
@@ -43,3 +43,64 @@ def test_simple(function_marked_bl_def_act_arr):
         LineType.blank_line,
         LineType._assert,
     ]
+
+
+# NOTE temporary tests to keep arrange blocks built the same with comments
+# until #148 is done
+
+@pytest.mark.parametrize('code_str', ['''
+def test_comment_after_act():
+    x = 1
+    y = 2
+
+    result = x + y
+    # Sum x and y
+
+    assert result == 2
+'''])
+def test_comment_after_act(function_marked_bl_def_act_arr):
+    """
+    Act block with trailing comment, comment is not marked as Assert
+    """
+    result = function_marked_bl_def_act_arr.mark_assert()
+
+    assert result == 1
+    assert function_marked_bl_def_act_arr.line_markers.types == [
+        LineType.func_def,
+        LineType.arrange,
+        LineType.arrange,
+        LineType.blank_line,
+        LineType.act,
+        LineType.act,
+        LineType.blank_line,
+        LineType._assert,
+    ]
+
+
+@pytest.mark.parametrize('code_str', ['''
+def test_comment_before_assert():
+    x = 1
+    y = 2
+
+    result = x + y
+
+    # Always 2
+    assert result == 2
+'''])
+def test_comment_start_assert(function_marked_bl_def_act_arr):
+    """
+    Comment at start of Assert is marked as part of that block
+    """
+    result = function_marked_bl_def_act_arr.mark_assert()
+
+    assert result == 2
+    assert function_marked_bl_def_act_arr.line_markers.types == [
+        LineType.func_def,
+        LineType.arrange,
+        LineType.arrange,
+        LineType.blank_line,
+        LineType.act,
+        LineType.blank_line,
+        LineType._assert,
+        LineType._assert,
+    ]
