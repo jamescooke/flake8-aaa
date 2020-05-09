26ae9fdee99fa45e750348a99b069c47eb96fab3
James Cooke <jamescooke@users.noreply.github.com>
2020-05-09 12:32:46 +0100

    Fix act block spacing: with is now part of arrange

--- examples/good/black/test_context_block_act.py
+++ examples/good/black/test_context_block_act.py
@@ -5,8 +5,8 @@ from .helpers import catch_signal
 
 def test(api_client, url):
     data = {"user_id": 0, "project_permission": "admin"}
-
     with catch_signal(user_perms_changed) as callback:
+
         result = api_client.put(url, data=data)
 
     assert result.status_code == 400
