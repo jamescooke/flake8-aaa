f1edfa8bcd66a25ba05cafa9fa8315959ee7e54b
James Cooke <jamescooke@users.noreply.github.com>
2021-04-22 21:18:14 +0100

    Bump to v0.11.2 (#181)

--- src/flake8_aaa/__about__.py
+++ src/flake8_aaa/__about__.py
@@ -1,6 +1,6 @@
 __short_name__ = 'aaa'
 __iam__ = 'flake8-{}'.format(__short_name__)
-__version__ = '0.11.1'
+__version__ = '0.11.2'
 
 __author__ = 'James Cooke'
 __copyright__ = '2018 - 2020, {}'.format(__author__)
