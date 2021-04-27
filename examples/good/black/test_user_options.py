4bffb2694256543e260a9dd647357fd5ef07bad4
James Cooke <jamescooke@users.noreply.github.com>
2021-04-27 22:11:25 +0100

    Update Black examples

--- examples/good/black/test_user_options.py
+++ examples/good/black/test_user_options.py
@@ -7,7 +7,9 @@ from tests.adapters.filesystem import FakeFileSystem
 def test_ini_file_reader(filename, contents, expected_options):
     settings.configure(
         FILE_SYSTEM=FakeFileSystem(
-            content_map={f"/path/to/folder/{filename}": contents},
+            content_map={
+                f"/path/to/folder/{filename}": contents,
+            },
             working_directory="/path/to/folder",
         )
     )
